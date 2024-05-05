2d243b3fa81ea9ac0f2d7a513fc44488122f893a
Anastasia R <anastasiarudakova@Anastasias-MacBook-Pro.local>
2024-05-05 21:28:18 +0800

    new file

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,26 +1,34 @@
 package src.main;
 
 import company.beans.Person;
+import company.extensions.InvalidAgeException;
 import company.implementations.EmailMessenger;
-import company.utils.MathUtils;
+import company.interfaces.Messenger;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            Person person = new Person("Person", 24);
-        } catch (Exception ex) {
-            System.out.println(ex);
-        }
+
+        Person[] people = new Person[5];
+
+        final int b = 10;
 
         try {
-            Person person = new Person("Person", 130);
-        } catch (Exception ex) {
-            System.out.println(ex);
+            people[0] = new Person("Anna", 20);
+            people[1] = new Person("Maria", 25);
+            people[2] = new Person("Amelia", 49);
+            people[3] = new Person("Bob", 24);
+            people[4] = new Person("Eva", 30);
+
+            for (Person person : people) {
+                int result = MathUtils.add(person.getAge(), b);
+                String message = "Wynik dla osoby " + person.getName() + ": " + result;
+
+                EmailMessenger emailMessenger = new EmailMessenger();
+                emailMessenger.sendMessage(message);
+            }
+        } catch (InvalidAgeException e) {
+
+            System.err.println("Invalid age: " + e.getMessage());
         }
-
-        EmailMessenger emailMessenger = new EmailMessenger();
-
-        String mathResult = String.valueOf(MathUtils.add(2, 2));
-        emailMessenger.sendMessage(mathResult);
     }
 }
