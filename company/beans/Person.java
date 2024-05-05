cd0eded0cd1833295c664c5aeb2bba63d4f80fe2
Anastasia R <anastasiarudakova@Anastasias-MacBook-Pro.local>
2024-05-05 20:31:43 +0800

    file

--- company/beans/Person.java
+++ company/beans/Person.java
@@ -1,6 +1,6 @@
 package company.beans;
 
-import company.exceptions.InvalidAgeException;
+import company.extensions.InvalidAgeException;
 
 public class Person {
     private String name;
@@ -33,3 +33,4 @@ public class Person {
        this.age = age;
     }
 }
+
